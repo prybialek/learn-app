f38b5825cea2573f278014748b245befda099f24
Pawel Rybialek <pawel.rybialek@gmail.com>
2019-04-01 22:42:44 +0200

    spring and oracle configuration

---
+++ src/main/java/com/prybialek/learnapp/LearnAppApplication.java
@@ -0,0 +1,13 @@
+package com.prybialek.learnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAppApplication.class, args);
+	}
+
+}
