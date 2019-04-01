240d2e690f05afefca5fa99ad81a0de5f4c2a690
Pawel Rybialek <pawel.rybialek@gmail.com>
2019-04-01 22:53:34 +0200

    [PaRy] shorter name for app

--- src/main/java/com/prybialek/learnapp/LearnApplication.java
+++ src/main/java/com/prybialek/learnapp/LearnApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnAppApplication {
+public class LearnApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnAppApplication.class, args);
+		SpringApplication.run(LearnApplication.class, args);
 	}
 
 }
